3fd02550ffb6287efa5c9f56586d241f4b5139c7
Marco Dal Boni <marco.dalboni@expandigroup.com>
2024-06-21 12:49:00 +0200

    added Todo

---
+++ src/todo.js
@@ -0,0 +1,38 @@
+export class Todo {
+
+    constructor(title, description, dueDate, priority, done) {
+
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    set title(value) { this._title = value; }
+
+    get title() { return this._title; }
+
+    set description(value) { this._description = value; }
+
+    get description() { return this._description; }
+
+    set dueDate(value) { this._dueDate = value; }
+
+    get dueDate() { return this._dueDate; }
+
+    set priority(value) { this._priority = priority; }
+
+    get priority() { return this._priority; }
+
+    set done(value) { this._done = value; }
+
+    get done() { return this._done; }
+
+    setDone = () => {
+        this._done = true;
+    }
+
+    printShort = () => {
+        return `${this.title}: due to ${this.dueDate}`;
+    }
+}
